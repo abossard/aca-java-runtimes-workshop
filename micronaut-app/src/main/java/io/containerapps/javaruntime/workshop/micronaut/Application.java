2d878b1b05778752f7d89aff3f379df6b079e684
Andre Bossard <anbossar@microsoft.com>
2025-03-10 16:57:11 +0100

    Implement CPU and memory consumption endpoints with statistics persistence; add application properties and tests

--- micronaut-app/src/main/java/io/containerapps/javaruntime/workshop/micronaut/Application.java
+++ micronaut-app/src/main/java/io/containerapps/javaruntime/workshop/micronaut/Application.java
@@ -1,10 +1,7 @@
 package io.containerapps.javaruntime.workshop.micronaut;
 
-import io.micronaut.core.annotation.Introspected;
 import io.micronaut.runtime.Micronaut;
-import jakarta.persistence.Entity;
 
-@Introspected(packages = "io.containerapps.javaruntime.workshop.domain", includedAnnotations = Entity.class)
 public class Application {
 
     public static void main(String[] args) {
--- quarkus-app/src/test/java/io/containerapps/javaruntime/workshop/quarkus/QuarkusResourceTest.java
+++ quarkus-app/src/test/java/io/containerapps/javaruntime/workshop/quarkus/QuarkusResourceTest.java
@@ -4,8 +4,12 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.*;
 @QuarkusTest
 class QuarkusResourceTest {
     @Test
@@ -14,7 +18,36 @@ class QuarkusResourceTest {
           .when().get("/quarkus")
           .then()
              .statusCode(200)
-             .body(is("Hello RESTEasy"));
+             .body(is("Quarkus: hello"));
     }
+    @Test
+    void testCpuWithDBAndDescEndpoint() {
+        given().param("iterations", 1).param("db", true).param("desc", "Java17")
+        .when().get("/quarkus/cpu")
+        .then()
+            .statusCode(200)
+            .body(startsWith("Quarkus: CPU consumption is done with"))
+            .body(not(containsString("Java17")))
+            .body(endsWith("The result is persisted in the database."));
+    }
+    @Test
+    void testMemoryWithDBAndDescEndpoint() {
+        given().param("bites", 1).param("db", true).param("desc", "Java17")
+        .when().get("/quarkus/memory")
+        .then()
+            .statusCode(200)
+            .body(startsWith("Quarkus: Memory consumption is done with"))
+            .body(not(containsString("Java17")))
+            .body(endsWith("The result is persisted in the database."));
+    }
+
+    @Test
+    void testStats() {
+        given()
+        .when().get("/quarkus/stats")
+        .then()
+            .statusCode(200);
+    }
+
 
 }
